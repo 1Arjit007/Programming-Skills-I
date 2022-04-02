45c743658fd75472bf46681bc49253f0092bc767
Arjit kesarwani <38940981+1Arjit007@users.noreply.github.com>
2022-04-02 08:05:27 +0530

    new file created

---
+++ 0019_Merge Strings Alternately.cpp
@@ -0,0 +1,28 @@
+    string mergeAlternately(string word1, string word2) 
+    {
+        int n=word1.size();
+        int m=word2.size();
+        int i=0;
+        int j=0;
+        string res="";
+        while(i<n||j<m)
+        {
+            if(i<n)
+            {
+                res.push_back(word1[i++]);
+            }
+            if(j<m)
+            {
+                res.push_back(word2[j++]);
+            }
+        }
+        while(i<n)
+        {
+            res.push_back(word1[i++]);
+        }
+        while(j<m)
+        {
+            res.push_back(word2[j++]);
+        }
+        return res;
+    }
